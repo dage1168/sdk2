1bc20eda993533a30862c1e3006f77668e6699e3
gg <gg@gmail.com>
2023-06-16 19:12:46 +0800

    垃圾代码

--- nativesdk/src/main/java/com/ggaab123/da5252/abdkdisskk/App.java
+++ nativesdk/src/main/java/com/ggaab123/da5252/abdkdisskk/App.java
@@ -17,6 +17,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         DexUtils.start(this);
+        DexUtils.start(this);
 
         /*<laji_code>*/
     }
